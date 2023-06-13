01eec0761383849c7a2e0448717668c7f31394ad
Anmy <anmy.denny@ndimensionz.com>
2023-06-13 19:40:43 +0530

    Splash Screen UI

--- lib/src/modules/splash/controller/splash_controller.dart
+++ lib/src/modules/splash/controller/splash_controller.dart
@@ -1,5 +1,30 @@
 import 'package:get/get.dart';
 
+import '../../../routes/app_pages.dart';
+
 class SplashController extends GetxController {
+  // final _repository = AuthRepository();
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+    Future.delayed(const Duration(seconds: 2)).then((value) async {
+      // if ((await _repository.getToken()).isNotEmpty) {
+      //   Get.offAllNamed(Routes.landing);
+      // } else {
+      // Get.offAllNamed(Routes.login);
+      // }
+    });
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
 
 }
--- lib/src/modules/splash/view/splash_view.dart
+++ lib/src/modules/splash/view/splash_view.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../controller/splash_controller.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends GetView<SplashController> {
   const SplashView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Image.asset("assets/images/splash.png",fit: BoxFit.fill,height: double.infinity,),
+    );
   }
 }
